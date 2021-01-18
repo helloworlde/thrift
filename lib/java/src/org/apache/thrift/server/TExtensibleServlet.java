7719e683df42127e54b9980960492d9cf7e1e04a
何惠民 <hehuimin03@meituan.com>
2021-01-18 11:36:28 +0800

    Servlet

--- lib/java/src/org/apache/thrift/server/TExtensibleServlet.java
+++ lib/java/src/org/apache/thrift/server/TExtensibleServlet.java
@@ -19,20 +19,6 @@
 
 package org.apache.thrift.server;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Map;
-
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.thrift.TException;
 import org.apache.thrift.TProcessor;
 import org.apache.thrift.protocol.TProtocol;
@@ -40,132 +26,162 @@ import org.apache.thrift.protocol.TProtocolFactory;
 import org.apache.thrift.transport.TIOStreamTransport;
 import org.apache.thrift.transport.TTransport;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
 /**
  * Servlet implementation class ThriftServer, that allows {@link TProcessor} and
  * {@link TProtocolFactory} to be supplied after the {@link #init()} method has
  * finished. <br>
+ * Thrift 服务端 Servlet 的实现，支持在完成 init 方法之后提供 TProcessor 和 TProtocolFactory
+ * <p>
  * Subclasses must implement the abstract methods that return the TProcessor and
  * two TProtocolFactory. Those methods are guaranteed to be called exactly once,
  * and that {@link ServletContext} is available.
+ * 子类必须实现重新方法返回 TProcessor 和 TProtocolFactory，这些方法保证只调用一次，
+ * 并且 ServletContext 是可用的
  */
 public abstract class TExtensibleServlet extends HttpServlet {
-  private static final long serialVersionUID = 1L;
 
-  private TProcessor processor;
+    private static final long serialVersionUID = 1L;
 
-  private TProtocolFactory inFactory;
+    private TProcessor processor;
 
-  private TProtocolFactory outFactory;
+    private TProtocolFactory inFactory;
 
-  private Collection<Map.Entry<String, String>> customHeaders;
+    private TProtocolFactory outFactory;
 
-  /**
-   * Returns the appropriate {@link TProcessor}. This will be called <b>once</b> just
-   * after the {@link #init()} method
-   * 
-   * @return the appropriate {@link TProcessor}
-   */
-  protected abstract TProcessor getProcessor();
+    private Collection<Map.Entry<String, String>> customHeaders;
 
-  /**
-   * Returns the appropriate in {@link TProtocolFactory}. This will be called
-   * <b>once</b> just after the {@link #init()} method
-   * 
-   * @return the appropriate in {@link TProtocolFactory}
-   */
-  protected abstract TProtocolFactory getInProtocolFactory();
+    /**
+     * Returns the appropriate {@link TProcessor}. This will be called <b>once</b> just
+     * after the {@link #init()} method
+     * 返回适当的 TProcessor，在 init 之后只调用一次
+     *
+     * @return the appropriate {@link TProcessor}
+     */
+    protected abstract TProcessor getProcessor();
 
-  /**
-   * Returns the appropriate out {@link TProtocolFactory}. This will be called
-   * <b>once</b> just after the {@link #init()} method
-   * 
-   * @return the appropriate out {@link TProtocolFactory}
-   */
-  protected abstract TProtocolFactory getOutProtocolFactory();
+    /**
+     * Returns the appropriate in {@link TProtocolFactory}. This will be called
+     * <b>once</b> just after the {@link #init()} method
+     * 返回适当的 TProtocolFactory，在 init 之后只调用一次
+     *
+     * @return the appropriate in {@link TProtocolFactory}
+     */
+    protected abstract TProtocolFactory getInProtocolFactory();
 
-  @Override
-  public final void init(ServletConfig config) throws ServletException {
-    super.init(config); //no-args init() happens here
-    this.processor = getProcessor();
-    this.inFactory = getInProtocolFactory();
-    this.outFactory = getOutProtocolFactory();
-    this.customHeaders = new ArrayList<Map.Entry<String, String>>();
+    /**
+     * Returns the appropriate out {@link TProtocolFactory}. This will be called
+     * <b>once</b> just after the {@link #init()} method
+     * 返回适当的 TProtocolFactory，在 init 之后只调用一次
+     *
+     * @return the appropriate out {@link TProtocolFactory}
+     */
+    protected abstract TProtocolFactory getOutProtocolFactory();
 
-    if (processor == null) {
-      throw new ServletException("processor must be set");
+    @Override
+    public final void init(ServletConfig config) throws ServletException {
+        super.init(config); //no-args init() happens here
+        // 初始化相应的 Processor 和 Factory
+        this.processor = getProcessor();
+        this.inFactory = getInProtocolFactory();
+        this.outFactory = getOutProtocolFactory();
+        this.customHeaders = new ArrayList<Map.Entry<String, String>>();
+
+        if (processor == null) {
+            throw new ServletException("processor must be set");
+        }
+        if (inFactory == null) {
+            throw new ServletException("inFactory must be set");
+        }
+        if (outFactory == null) {
+            throw new ServletException("outFactory must be set");
+        }
     }
-    if (inFactory == null) {
-      throw new ServletException("inFactory must be set");
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    @Override
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
+        TTransport inTransport = null;
+        TTransport outTransport = null;
+
+        try {
+            response.setContentType("application/x-thrift");
+
+            // 如果有自定义的 header，则添加到响应中
+            if (null != this.customHeaders) {
+                for (Map.Entry<String, String> header : this.customHeaders) {
+                    response.addHeader(header.getKey(), header.getValue());
+                }
+            }
+
+            InputStream in = request.getInputStream();
+            OutputStream out = response.getOutputStream();
+
+            TTransport transport = new TIOStreamTransport(in, out);
+            inTransport = transport;
+            outTransport = transport;
+
+            TProtocol inProtocol = inFactory.getProtocol(inTransport);
+            TProtocol outProtocol = inFactory.getProtocol(outTransport);
+
+            // 处理请求
+            processor.process(inProtocol, outProtocol);
+            out.flush();
+        } catch (TException te) {
+            throw new ServletException(te);
+        }
     }
-    if (outFactory == null) {
-      throw new ServletException("outFactory must be set");
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    @Override
+    protected void doGet(HttpServletRequest req,
+                         HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
     }
-  }
 
-  /**
-   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-   *      response)
-   */
-  @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-    TTransport inTransport = null;
-    TTransport outTransport = null;
+    /**
+     * 添加自定义的 Header
+     */
+    public void addCustomHeader(final String key, final String value) {
+        this.customHeaders.add(new Map.Entry<String, String>() {
+            public String getKey() {
+                return key;
+            }
 
-    try {
-      response.setContentType("application/x-thrift");
+            public String getValue() {
+                return value;
+            }
 
-      if (null != this.customHeaders) {
-	for (Map.Entry<String, String> header : this.customHeaders) {
-	  response.addHeader(header.getKey(), header.getValue());
-	}
-      }
-
-      InputStream in = request.getInputStream();
-      OutputStream out = response.getOutputStream();
-
-      TTransport transport = new TIOStreamTransport(in, out);
-      inTransport = transport;
-      outTransport = transport;
-
-      TProtocol inProtocol = inFactory.getProtocol(inTransport);
-      TProtocol outProtocol = inFactory.getProtocol(outTransport);
-
-      processor.process(inProtocol, outProtocol);
-      out.flush();
-    } catch (TException te) {
-      throw new ServletException(te);
+            public String setValue(String value) {
+                return null;
+            }
+        });
     }
-  }
 
-  /**
-   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-   *      response)
-   */
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-    doPost(req, resp);
-  }
-
-  public void addCustomHeader(final String key, final String value) {
-    this.customHeaders.add(new Map.Entry<String, String>() {
-      public String getKey() {
-	return key;
-      }
-
-      public String getValue() {
-	return value;
-      }
-
-      public String setValue(String value) {
-	return null;
-      }
-    });
-  }
-
-  public void setCustomHeaders(Collection<Map.Entry<String, String>> headers) {
-    this.customHeaders.clear();
-    this.customHeaders.addAll(headers);
-  }
+    /**
+     * 设置自定义的 header
+     */
+    public void setCustomHeaders(Collection<Map.Entry<String, String>> headers) {
+        this.customHeaders.clear();
+        this.customHeaders.addAll(headers);
+    }
 }
--- lib/java/src/org/apache/thrift/server/TServlet.java
+++ lib/java/src/org/apache/thrift/server/TServlet.java
@@ -1,17 +1,5 @@
 package org.apache.thrift.server;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Map;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.thrift.TException;
 import org.apache.thrift.TProcessor;
 import org.apache.thrift.protocol.TProtocol;
@@ -19,101 +7,126 @@ import org.apache.thrift.protocol.TProtocolFactory;
 import org.apache.thrift.transport.TIOStreamTransport;
 import org.apache.thrift.transport.TTransport;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
 /**
  * Servlet implementation class ThriftServer
+ * Thrift 服务端的 Servlet 实现
  */
 public class TServlet extends HttpServlet {
 
-  private final TProcessor processor;
+    private final TProcessor processor;
 
-  private final TProtocolFactory inProtocolFactory;
+    private final TProtocolFactory inProtocolFactory;
 
-  private final TProtocolFactory outProtocolFactory;
+    private final TProtocolFactory outProtocolFactory;
 
-  private final Collection<Map.Entry<String, String>> customHeaders;
+    private final Collection<Map.Entry<String, String>> customHeaders;
 
-  /**
-   * @see HttpServlet#HttpServlet()
-   */
-  public TServlet(TProcessor processor, TProtocolFactory inProtocolFactory,
-      TProtocolFactory outProtocolFactory) {
-    super();
-    this.processor = processor;
-    this.inProtocolFactory = inProtocolFactory;
-    this.outProtocolFactory = outProtocolFactory;
-    this.customHeaders = new ArrayList<Map.Entry<String, String>>();
-  }
-
-  /**
-   * @see HttpServlet#HttpServlet()
-   */
-  public TServlet(TProcessor processor, TProtocolFactory protocolFactory) {
-    this(processor, protocolFactory, protocolFactory);
-  }
-
-  /**
-   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-   *      response)
-   */
-  @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-
-    TTransport inTransport = null;
-    TTransport outTransport = null;
-
-    try {
-      response.setContentType("application/x-thrift");
-
-      if (null != this.customHeaders) {
-        for (Map.Entry<String, String> header : this.customHeaders) {
-          response.addHeader(header.getKey(), header.getValue());
-        }
-      }
-      InputStream in = request.getInputStream();
-      OutputStream out = response.getOutputStream();
-
-      TTransport transport = new TIOStreamTransport(in, out);
-      inTransport = transport;
-      outTransport = transport;
-
-      TProtocol inProtocol = inProtocolFactory.getProtocol(inTransport);
-      TProtocol outProtocol = outProtocolFactory.getProtocol(outTransport);
-
-      processor.process(inProtocol, outProtocol);
-      out.flush();
-    } catch (TException te) {
-      throw new ServletException(te);
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TServlet(TProcessor processor,
+                    TProtocolFactory inProtocolFactory,
+                    TProtocolFactory outProtocolFactory) {
+        super();
+        this.processor = processor;
+        this.inProtocolFactory = inProtocolFactory;
+        this.outProtocolFactory = outProtocolFactory;
+        this.customHeaders = new ArrayList<Map.Entry<String, String>>();
     }
-  }
 
-  /**
-   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-   *      response)
-   */
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-    doPost(request, response);
-  }
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TServlet(TProcessor processor, TProtocolFactory protocolFactory) {
+        this(processor, protocolFactory, protocolFactory);
+    }
 
-  public void addCustomHeader(final String key, final String value) {
-    this.customHeaders.add(new Map.Entry<String, String>() {
-      public String getKey() {
-        return key;
-      }
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    @Override
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
 
-      public String getValue() {
-        return value;
-      }
+        TTransport inTransport = null;
+        TTransport outTransport = null;
 
-      public String setValue(String value) {
-        return null;
-      }
-    });
-  }
+        try {
+            // 设置响应类型
+            response.setContentType("application/x-thrift");
 
-  public void setCustomHeaders(Collection<Map.Entry<String, String>> headers) {
-    this.customHeaders.clear();
-    this.customHeaders.addAll(headers);
-  }
+            // 如果有自定义的 Header，则加入到响应中
+            if (null != this.customHeaders) {
+                for (Map.Entry<String, String> header : this.customHeaders) {
+                    response.addHeader(header.getKey(), header.getValue());
+                }
+            }
+
+            InputStream in = request.getInputStream();
+            OutputStream out = response.getOutputStream();
+
+            TTransport transport = new TIOStreamTransport(in, out);
+            inTransport = transport;
+            outTransport = transport;
+
+            TProtocol inProtocol = inProtocolFactory.getProtocol(inTransport);
+            TProtocol outProtocol = outProtocolFactory.getProtocol(outTransport);
+
+            // 处理请求
+            processor.process(inProtocol, outProtocol);
+
+            // 输出响应
+            out.flush();
+        } catch (TException te) {
+            throw new ServletException(te);
+        }
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    /**
+     * 添加自定义的 Header
+     */
+    public void addCustomHeader(final String key, final String value) {
+        this.customHeaders.add(new Map.Entry<String, String>() {
+            public String getKey() {
+                return key;
+            }
+
+            public String getValue() {
+                return value;
+            }
+
+            public String setValue(String value) {
+                return null;
+            }
+        });
+    }
+
+    /**
+     * 设置自定义的 header
+     */
+    public void setCustomHeaders(Collection<Map.Entry<String, String>> headers) {
+        this.customHeaders.clear();
+        this.customHeaders.addAll(headers);
+    }
 }
